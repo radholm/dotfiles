29b5fd51c3dec0e33e0b6a65c7d5ddb97354076e
Fredrik Radholm <fredrik.radholm@tietoevry.com>
2023-04-15 19:49:17 +0200

    Added spaceship config

---
+++ zsh/.spaceshiprc.zsh
@@ -0,0 +1,17 @@
+SPACESHIP_PROMPT_ORDER=(
+  time           # Time stamps section
+  user           # Username section
+  dir            # Current directory section
+  host           # Hostname section
+  git            # Git section (git_branch + git_status)
+  async          # Async jobs indicator
+  line_sep       # Line break
+  battery        # Battery level and status
+  jobs           # Background jobs indicator
+  exit_code      # Exit code section
+  sudo           # Sudo indicator
+  char           # Prompt character
+)
+SPACESHIP_PROMPT_DEFAULT_SUFFIX=" "
+SPACESHIP_CHAR_SYMBOL=">"
+SPACESHIP_CHAR_SUFFIX=" "
